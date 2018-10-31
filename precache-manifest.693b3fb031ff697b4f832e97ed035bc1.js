b480d956574f417e821cf86b24d18433ab01dc29
Mike <mikye777@gmail.com>
2018-10-31 18:17:48 +0200

    Updates

---
+++ precache-manifest.693b3fb031ff697b4f832e97ed035bc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "801a772f79fc5b0c1058",
+    "url": "/mike7yes/drum-pads/static/js/runtime~main.801a772f.js"
+  },
+  {
+    "revision": "403f5ffbd885c1c6c03c",
+    "url": "/mike7yes/drum-pads/static/js/main.403f5ffb.chunk.js"
+  },
+  {
+    "revision": "d55a4da96304438d171c",
+    "url": "/mike7yes/drum-pads/static/js/1.d55a4da9.chunk.js"
+  },
+  {
+    "revision": "403f5ffbd885c1c6c03c",
+    "url": "/mike7yes/drum-pads/static/css/main.e9a9f738.chunk.css"
+  },
+  {
+    "revision": "2dcb1942909e9a72fdb8d0e7406326e3",
+    "url": "/mike7yes/drum-pads/index.html"
+  }
+];
